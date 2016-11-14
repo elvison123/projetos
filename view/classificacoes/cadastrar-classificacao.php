98ae8e03ed1a8e10b3d4872f759dde0725070ad7
elvison123 <elvisondemelo@hotmail.com>
2016-11-14 16:44:30 -0200

    classificacao consertar erros após mesclar

--- view/classificacoes/cadastrar-classificacao.php
+++ view/classificacoes/cadastrar-classificacao.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 session_start();
 if(!isset($_SESSION["nomeusuario"])){
@@ -12,7 +11,7 @@ if(!isset($_SESSION["nomeusuario"])){
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Home</title>
+        <title>Cadastrar</title>
 <!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
 <!--        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>-->        
         <script src="../js/jquery-3.1.0.min.js"></script>
@@ -23,13 +22,12 @@ if(!isset($_SESSION["nomeusuario"])){
         <script src="../js/exemplo_1.js"></script>
         <script src="../js/exemplo_2.js"></script>     
         <script src="../js/exemplo_3.js"></script> 
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
          <!-- Bootstrap -->
         <link href="../css/bootstrap.css" rel="stylesheet">
 
     </head>
-    <ul class="nav nav-pills">
+    
+   <ul class="nav nav-pills">
         <li role="presentation" class="active"><a href="../home/home-usuario.php">Home</a></li>
         <!-- <li role="presentation"><a href="#">Messages</a></li>-->
         <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
@@ -43,12 +41,24 @@ if(!isset($_SESSION["nomeusuario"])){
 
             </ul></li>
             <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+                                                    aria-expanded="false"> Fornecedores <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+                <li><a href="../fornecedores/cadastrar-fornecedor.php">Cadastrar Fornecedores</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="../../controller/FornecedorController.php?acao=buscartodos" >Mostrar Fornecedores Cadastrados</a></li>
+                <li role="separator" class="divider"></li>
+
+            </ul></li>
+            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
                                                     aria-expanded="false"> Títulos <span class="caret"></span>
             </a>
             <ul class="dropdown-menu">
                 <li><a href="../../controller/TituloController.php?acao=listarclientes">Cadastrar Títulos a receber</a></li>
+                <li><a href="../../controller/TituloController.php?acao=listartitulos"> Mostrar todos os títulos a receber</a></li>
                 <li role="separator" class="divider"></li>
-                <li><a href="../../controller/TituloController.php?acao=listartitulos"> Mostrar todos os títulos cadastrados</a></li>
+                <li><a href="../../controller/TituloPagarController.php?acao=listarfornecedor">Cadastrar Títulos a pagar</a></li>
+                <li><a href="../../controller/TituloPagarController.php?acao=listartitulos"> Mostrar todos os títulos a pagar</a></li>
                 
 
             </ul></li>
@@ -73,8 +83,11 @@ if(!isset($_SESSION["nomeusuario"])){
                 <li><a href="../../controller/ModuloController.php?acao=paginamodulo">Cadastrar Modulos</a></li>
                 <li><a href="../../controller/ModuloController.php?acao=listarmodulo">Listar Modulos </a></li>
                 <li role="separator" class="divider"></li>
-                <li><a href="../../controller/ModuloController.php?acao=paginafuncao">Cadastrar função</a></li>
-                <li><a href="../../controller/ModuloController.php?acao=listarfuncoes">Listar Funções cadastradas</a></li>
+                <li><a href="../../controller/FuncaoModuloController.php?acao=paginafuncao">Cadastrar função</a></li>
+                <li><a href="../../controller/FuncaoModuloController.php?acao=listarfuncoes">Listar Funções cadastradas</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="../../view/prioridade/cadastrar-prioridade.php">Cadastrar Prioridade</a></li>
+                <li><a href="../../controller/PrioridadeController.php?acao=buscartodos">Listar Prioridades Cadastradas</a></li>
                 
 
             </ul></li>
@@ -202,212 +215,3 @@ if(!isset($_SESSION["nomeusuario"])){
 
 </body>
 </html>
-=======
-<?php 
-session_start();
-if(!isset($_SESSION["nomeusuario"])){
-    header('Location: LoginUsuario.php');
-   
-}
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Cadastrar</title>
-<!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
-<!--        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>-->        
-        <script src="../js/jquery-3.1.0.min.js"></script>
-        <script src="../js/jquery.validate.min.js"></script>
-        <script src="../js/jquery.maskedinput.min.js"></script>
-        <script src="../js/bootstrap.min.js"></script>
-        <!--<script src="../js/valida_cpf_cnpj.js"></script>-->
-        <script src="../js/exemplo_1.js"></script>
-        <script src="../js/exemplo_2.js"></script>     
-        <script src="../js/exemplo_3.js"></script> 
-         <!-- Bootstrap -->
-        <link href="../css/bootstrap.css" rel="stylesheet">
-
-    </head>
-    
-   <ul class="nav nav-pills">
-        <li role="presentation" class="active"><a href="../home/home-usuario.php">Home</a></li>
-        <!-- <li role="presentation"><a href="#">Messages</a></li>-->
-        <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Clientes <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="../clientes/cadastrar-cliente.php">Cadastrar Clientes</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/ClienteController.php?acao=buscartodos" >Mostrar Clientes Cadastrados</a></li>
-                <li role="separator" class="divider"></li>
-
-            </ul></li>
-            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Fornecedores <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="../fornecedores/cadastrar-fornecedor.php">Cadastrar Fornecedores</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/FornecedorController.php?acao=buscartodos" >Mostrar Fornecedores Cadastrados</a></li>
-                <li role="separator" class="divider"></li>
-
-            </ul></li>
-            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Títulos <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="../../controller/TituloController.php?acao=listarclientes">Cadastrar Títulos a receber</a></li>
-                <li><a href="../../controller/TituloController.php?acao=listartitulos"> Mostrar todos os títulos a receber</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/TituloPagarController.php?acao=listarfornecedor">Cadastrar Títulos a pagar</a></li>
-                <li><a href="../../controller/TituloPagarController.php?acao=listartitulos"> Mostrar todos os títulos a pagar</a></li>
-                
-
-            </ul></li>
-            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Usuários <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="../usuarios/cadastrar-usuario.php">Cadastrar Usuários</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/UsuarioController.php?acao=listarusuarios"> Mostrar todos os Usuários cadastrados</a></li>
-                
-
-            </ul></li>
-            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Chamados <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li role="separator" class="divider"></li>
-                <li><a href="../../view/classificacoes/cadastrar-classificacao.php">Cadastrar Classificação</a></li>
-                <li><a href="../../controller/ClassificacaoController.php?acao=buscartodos">Listar Classificacoes Cadastradas</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/ModuloController.php?acao=paginamodulo">Cadastrar Modulos</a></li>
-                <li><a href="../../controller/ModuloController.php?acao=listarmodulo">Listar Modulos </a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/FuncaoModuloController.php?acao=paginafuncao">Cadastrar função</a></li>
-                <li><a href="../../controller/FuncaoModuloController.php?acao=listarfuncoes">Listar Funções cadastradas</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../view/prioridade/cadastrar-prioridade.php">Cadastrar Prioridade</a></li>
-                <li><a href="../../controller/PrioridadeController.php?acao=buscartodos">Listar Prioridades Cadastradas</a></li>
-                
-
-            </ul></li>
-            
-            <ul class="nav navbar-nav navbar-right">
-            
-            <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown"><?php  if (isset($_SESSION['nomeusuario'])){echo "Olá, " . $_SESSION["nomeusuario"];}; ?><b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#"></a></li>
-                    <li><a href="#">Configurações do perfil</a></li>
-                    
-                    
-                </ul>
-            </li>
-            <li><a href="../../controller/LoginUsuarioController.php?acao=logout">Sair</a></li>
-        </ul>
-    </ul>
-
-
-
-    <div class="container">
-        <?php if (isset($_GET['status'])) { ?>
-                <div class="alert alert-info">
-                    <center><?php echo $_GET['status']; ?></center>
-                </div>
-           
-
-        <?php }; ?>
-        
-        
-    <div class="row">
-        <div class="col-xs-4">
-            <h1 class="h1">Cadastrar Classificações</h1>
-        </div>
-    </div>
-
-    <div class="container">
-        <form action="../../controller/ClassificacaoController.php" method="GET" id="formcad" onclick="" novalidate="" >
-            <input type="hidden" name="acao" value="cadastrar">
-
-            <div class="form-group row">
-                <label for="nome" class="col-xs-2 col-form-label"  >Nome da Classificação</label>
-                <div class="col-xs-8">
-                    <input type="name" class="form-control" name="nome"   id="nome" required maxlength="50" placeholder="Digite o Nome da Classificação">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-2"></div>
-                <div class="col-xs-6">
-                <input class="btn btn-success" type="submit" onclick="clicked"id="myBtn" value="Cadastrar">
-                </div>
-                <div class="col-xs-2">
-                    <a class="btn btn-default" href="../../controller/ClassificacaoController.php?acao=buscartodos">Exibir Cadastro</a>
-                </div>
-            </div>
-            </form>
-
-            </div>
-
-
-            <script>
-                $(document).ready(function () {
-                    $("#formcad").validate({
-                        rules: {
-                            nome: {
-                                required: true,
-                                minlength: 2
-
-                            }
-
-                        },
-                        messages: {
-                            nome: {
-                                required: "Preenchimento obrigatório!",
-                                minlength: "Deve conter no minimo 2 caracteres!"
-                            }
-
-                        }
-
-                    });
-
-                });
-                
-
-
-            </script>
-
-
-
-
-    </div>
-    <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog">
-
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Modal Header</h4>
-                </div>
-                <div class="modal-body">
-                    <p>Some text in the modal.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-
-        </div>
-    </div>
-     
-
-
-
-    
-</body>
-</html>
->>>>>>> origin/master
--- view/classificacoes/listar-classificacao.php
+++ view/classificacoes/listar-classificacao.php
@@ -1,185 +1,3 @@
-<<<<<<< HEAD
-<?php 
-session_start();
-if(!isset($_SESSION["nomeusuario"])){
-    header('Location: LoginUsuario.php');
-   
-}
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Home</title>
-<!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
-<!--        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>-->        
-        <script src="../js/jquery-3.1.0.min.js"></script>
-        <script src="../js/jquery.validate.min.js"></script>
-        <script src="../js/jquery.maskedinput.min.js"></script>
-        <script src="../js/bootstrap.min.js"></script>
-        <!--<script src="../js/valida_cpf_cnpj.js"></script>-->
-        <script src="../js/exemplo_1.js"></script>
-        <script src="../js/exemplo_2.js"></script>     
-        <script src="../js/exemplo_3.js"></script> 
-         <!-- Bootstrap -->
-        <link href="../css/bootstrap.css" rel="stylesheet">
-
-    </head>
-    <ul class="nav nav-pills">
-        <li role="presentation" class="active"><a href="../home/home-usuario.php">Home</a></li>
-        <!-- <li role="presentation"><a href="#">Messages</a></li>-->
-        <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Clientes <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="../clientes/cadastrar-cliente.php">Cadastrar Clientes</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/ClienteController.php?acao=buscartodos" >Mostrar Clientes Cadastrados</a></li>
-                <li role="separator" class="divider"></li>
-
-            </ul></li>
-            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Títulos <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="../../controller/TituloController.php?acao=listarclientes">Cadastrar Títulos a receber</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/TituloController.php?acao=listartitulos"> Mostrar todos os títulos cadastrados</a></li>
-                
-
-            </ul></li>
-            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Usuários <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="../usuarios/cadastrar-usuario.php">Cadastrar Usuários</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/UsuarioController.php?acao=listarusuarios"> Mostrar todos os Usuários cadastrados</a></li>
-                
-
-            </ul></li>
-            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                                                    aria-expanded="false"> Chamados <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li role="separator" class="divider"></li>
-                <li><a href="../../view/classificacoes/cadastrar-classificacao.php">Cadastrar Classificação</a></li>
-                <li><a href="../../controller/ClassificacaoController.php?acao=buscartodos">Listar Classificacoes Cadastradas</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/ModuloController.php?acao=paginamodulo">Cadastrar Modulos</a></li>
-                <li><a href="../../controller/ModuloController.php?acao=listarmodulo">Listar Modulos </a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="../../controller/ModuloController.php?acao=paginafuncao">Cadastrar função</a></li>
-                <li><a href="../../controller/ModuloController.php?acao=listarfuncoes">Listar Funções cadastradas</a></li>
-                
-
-            </ul></li>
-            
-            <ul class="nav navbar-nav navbar-right">
-            
-            <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown"><?php  if (isset($_SESSION['nomeusuario'])){echo "Olá, " . $_SESSION["nomeusuario"];}; ?><b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#"></a></li>
-                    <li><a href="#">Configurações do perfil</a></li>
-                    
-                    
-                </ul>
-            </li>
-            <li><a href="../../controller/LoginUsuarioController.php?acao=logout">Sair</a></li>
-        </ul>
-    </ul>
-
-
-  <div class="container">
-      <?php if (isset($_GET['status'])) { ?>
-                <div class="alert alert-info">
-                    <center><?php echo $_GET['status']; ?></center>
-                </div>
-           
-
-        <?php }; ?> 
-         
-      
-    <div class="row">
-        <div class="col-xs-4">
-            <h1 class="h1">Listar Classificações</h1>
-        </div>
-    </div>
-    <div class="container">
-        <table class="table table-bordered table-hover">
-            
-            <thead>
-                <tr class="info">
-                    <td><strong>ID</strong></td>
-                    <td><strong>NOME DAS CLASSIFICAÇÕES</strong></td>
-                    <td><strong>TIPO</strong></td>
-                    <td><strong>DELETAR</strong></td>
-                    <td><strong>EDITAR</strong></td>
-                    
-                    
-                </tr>
-            </thead>    
-
-            
-            <?php if (isset($_SESSION["classificacao1"])){foreach ($_SESSION["classificacao1"] as $classificacao):?>     
-            <tbody>        
-                <tr>
-                    <td><?php echo $classificacao["id_classificacao"];?></td>
-                    <td><?php echo $classificacao["nome"];?></td>
-                    <td><?php echo 'S'== $classificacao["tipo"]?'Software':'Hardware';?></td>
-                        
-                    <td><button class="delete btn btn-danger" data-nome ="<?php echo $classificacao["nome"]; ?>" data-id="<?php echo $classificacao["id_classificacao"]; ?>"data-target="#myModal">Excluir</td>
-                    <td><a class="btn btn-default" href="editar-classificacao.php?id_classificacao=<?php echo $classificacao["id_classificacao"];?>&nome=<?php echo $classificacao["nome"];?>" role="button">Editar</a></td>
-                </tr>
-            </tbody>
-                    
-            <?php endforeach;};?>   
-            
-        </table>
-        </div>
-        <script>
-        $('.delete').on('click', function () {
-                    var nome = $(this).data('nome'); 
-                    var id = $(this).data('id');
-                    $('span.nome').text(nome); 
-                    $('a.delete-yes').attr('href', '../../controller/ClassificacaoController.php?acao=deletar&id=' + id); 
-                    $('#myModal').modal('show'); 
-                });
-        </script>
-        
-        
-        
-        <!-- Modal -->
-        <div id="myModal" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Confirmar Ação</h4>
-                    </div>
-                    <div class="modal-body">
-                        Tem certeza que deseja excluir a seguinte Classificação <strong><span class="nome"></span></strong>?
-                    </div>
-                    <div class="modal-footer">
-                        <a href="#" type="button" class="btn btn-default delete-yes">Sim</a>
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Não</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        
-        
-        
-
-
-
-
-    </body>
-</html>
-=======
 <?php 
 session_start();
 if(!isset($_SESSION["nomeusuario"])){
@@ -288,6 +106,7 @@ if(!isset($_SESSION["nomeusuario"])){
         </ul>
     </ul>
 
+
   <div class="container">
       <?php if (isset($_GET['status'])) { ?>
                 <div class="alert alert-info">
@@ -310,8 +129,10 @@ if(!isset($_SESSION["nomeusuario"])){
                 <tr class="info">
                     <td><strong>ID</strong></td>
                     <td><strong>NOME DAS CLASSIFICAÇÕES</strong></td>
+                    <td><strong>TIPO</strong></td>
                     <td><strong>DELETAR</strong></td>
                     <td><strong>EDITAR</strong></td>
+                    
                     
                 </tr>
             </thead>    
@@ -322,6 +143,8 @@ if(!isset($_SESSION["nomeusuario"])){
                 <tr>
                     <td><?php echo $classificacao["id_classificacao"];?></td>
                     <td><?php echo $classificacao["nome"];?></td>
+                    <td><?php echo 'S'== $classificacao["tipo"]?'Software':'Hardware';?></td>
+                        
                     <td><button class="delete btn btn-danger" data-nome ="<?php echo $classificacao["nome"]; ?>" data-id="<?php echo $classificacao["id_classificacao"]; ?>"data-target="#myModal">Excluir</td>
                     <td><a class="btn btn-default" href="editar-classificacao.php?id_classificacao=<?php echo $classificacao["id_classificacao"];?>&nome=<?php echo $classificacao["nome"];?>" role="button">Editar</a></td>
                 </tr>
@@ -371,4 +194,3 @@ if(!isset($_SESSION["nomeusuario"])){
 
     </body>
 </html>
->>>>>>> origin/master
